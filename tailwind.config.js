dbee6cab659f6e4469ded44d0512d45b9abf6e2b
Dilip-777 <dilip7989773@gmail.com>
2024-01-30 13:31:25 +0530

    dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  // darkMode: "class",
+  darkMode: "class",
 
   theme: {
     fontFamily: {

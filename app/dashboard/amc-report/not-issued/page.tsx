57dd67a444b6248b26981fcf39789ac0f5fc7b00
Dilip-777 <dilip7989773@gmail.com>
2024-02-26 18:53:31 +0530

    type error fix

--- app/dashboard/amc-report/not-issued/page.tsx
+++ app/dashboard/amc-report/not-issued/page.tsx
@@ -2,19 +2,22 @@ import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
 
 import { Metadata } from "next";
 import CustomerTable from "../../customers/table";
+import { db } from "@/db";
+import { customers } from "@/db/schema";
 export const metadata: Metadata = {
   title: "Customers",
   description: "This is Customers page",
   // other metadata
 };
 
-const Customers = () => {
+const Customers = async () => {
+  const data = await db.select().from(customers);
   return (
     <>
       <Breadcrumb pageName="Customers" nav={false} />
 
       <div className="flex flex-col gap-10">
-        <CustomerTable />
+        <CustomerTable data={data} />
       </div>
     </>
   );

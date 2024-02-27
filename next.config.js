f14dd6dc1b18e25a48446726ced5aa1e76cc23c5
Dilip-777 <dilip7989773@gmail.com>
2024-02-28 03:00:08 +0530

    next config setting

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/dashboard",
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/dashboard",
+        permanent: true, // Set to true for a permanent redirect (301)
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;

13e524119d499cebf277927542253d6cd94c6525
Dilip-777 <dilip7989773@gmail.com>
2024-02-28 02:55:35 +0530

    acm creation

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,6 @@ import { useState, useEffect } from "react";
 import Loader from "@/components/common/Loader";
 import Provider from "./_trpc/Provider";
 import useColorMode from "@/hooks/useColorMode";
-import { redirect } from "next/navigation";
 
 export default function RootLayout({
   children,
@@ -22,24 +21,25 @@ export default function RootLayout({
     setTimeout(() => setLoading(false), 1000);
   }, []);
 
-  return redirect("/dashboard");
-  // <html lang="en">
-  //   <body suppressHydrationWarning={true}>
-  //     <div className="bg-boxdark-2 text-bodydark">
-  //       {loading ? (
-  //         <Loader />
-  //       ) : (
-  //         <div className="flex h-screen overflow-hidden">
-  //           <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
-  //             <main>
-  //               <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10 bg-black">
-  //                 <Provider>{children}</Provider>
-  //               </div>
-  //             </main>
-  //           </div>
-  //         </div>
-  //       )}
-  //     </div>
-  //   </body>
-  // </html>
+  return (
+    <html lang="en">
+      <body suppressHydrationWarning={true}>
+        <div className="bg-boxdark-2 text-bodydark">
+          {loading ? (
+            <Loader />
+          ) : (
+            <div className="flex h-screen overflow-hidden">
+              <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+                <main>
+                  <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10 bg-black">
+                    <Provider>{children}</Provider>
+                  </div>
+                </main>
+              </div>
+            </div>
+          )}
+        </div>
+      </body>
+    </html>
+  );
 }
